96a94366222541b6630ce4462882b715dfaa8792
Abigya Ayele <abigya.ayele.12@gmail.com>
2023-12-20 23:15:54 +0300

    update for subject metadata

--- invenio_administration/views/publish.py
+++ invenio_administration/views/publish.py
@@ -95,9 +95,9 @@ def publish_function():
             get_rights = data.get('metadata', {}).get('rights', [])
             for right in get_rights:
                 rights_list.append(right)
-            get_subject = data.get('metadata', {}).get('subjects', [])
+            get_subject = data.get('metadata', {}).get('subject', [])
             for subject in get_subject:
-                subjects_list.append(right)
+                subjects_list.append(subject)
             rectitle = data.get('metadata', {}).get('title','')
             
 
